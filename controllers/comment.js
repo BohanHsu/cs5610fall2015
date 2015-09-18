dcb3c89db6cd3fbc4429d118cf60f475cc4932e5
BohanHsu <ilovebarce@gmail.com>
2015-09-18 18:32:17 -0400

    update demo works

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express()
 var models = require('../models')
+var multer = require('multer')
+var fs = require('fs')
+var path = require('path')
+var mkdirp = require('mkdirp')
 
 app.get('/', function (req, res) {
   models.Comment.find({}).exec().then(function (comments) {
@@ -25,7 +29,44 @@ app.post('/', function (req, res) {
       })
     })
   })
+})
 
+app.use(multer({ dest: __dirname + '/../tmp' }).single('image'))
+app.post('/image', function (req, res) {
+  fs.readFile(req.file.path, function (err, data) {
+    if (!err) {
+      var userName = null
+      if (req.user) {
+        userName = req.user.local.username
+      } else {
+        userName = 'guest'
+      }
+      var newPath = path.join(__dirname, '../public/uploads', userName, req.file.filename)
+
+      mkdirp(path.dirname(newPath), function (err) {
+        if (err)
+          console.log(err)
+
+        fs.writeFile(newPath, data, function (err) {
+          if (err)
+            console.log(err)
+          fs.unlink(req.file.path, function (err) {
+            var comment = new models.Comment({
+              content: req.body.content,
+              imageUrl: path.join('uploads', userName, req.file.filename)
+            })
+
+            comment.save(function (err, comment) {
+              if (err) {
+                console.log(err)
+              }
+              res.redirect('/comments')
+            })
+          })
+        })
+      })
+    }
+  })
 })
 
 module.exports = app
--- models/comment.js
+++ models/comment.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose')
 
 var commentSchema = new mongoose.Schema({
-  content: 'string'
+  content: 'string',
+  imageUrl: 'string'
 })
 
 module.exports = mongoose.model('Comment', commentSchema)
