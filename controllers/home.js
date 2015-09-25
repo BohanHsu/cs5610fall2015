6ab0266d5abef3b3927868412e85f8ad5a4d7e46
BohanHsu <ilovebarce@gmail.com>
2015-09-25 16:53:19 -0400

    add pretty to jade

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+app.locals.pretty = true
 
 app.get('/', function (req, res) {
   res.render('home', {title: 'Hello world', message: 'NEU CS5610 Bohan Xu'})
--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,3 @@
-//var express = require('express')
-//var app = express()
-//var bodyParser = require('body-parser')
-//
-//app.use(bodyParser.urlencoded({ extended: false }))
-//app.use(bodyParser.json())
-//
-//app.use('/', require('./home'))
-//app.use('/comments', require('./comment'))
-//app.use('/login', require('./login'))
-//
-//module.exports = app
-
 var bodyParser = require('body-parser')
 
 module.exports = function (app, passport) {
